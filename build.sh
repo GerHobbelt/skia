08fe1bd4422376dbafcedb15e8314635b481e47f
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2025-04-08 19:58:28 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ fi
 rm -rf out/build-bundle || true
 rm -rf out/build-system || true
 
-git submodule --init update
+git submodule update --init
 python3 tools/git-sync-deps
 
 bin/gn gen out/build-bundle --args='is_official_build=true is_debug=false cc="clang" cxx="clang++" extra_cflags=["-Wno-error"] target_os="linux" target_cpu="x64" skia_use_system_expat=false skia_use_system_freetype2=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false skia_use_system_icu=false skia_use_system_harfbuzz=false skia_use_dng_sdk=false'
